355fe143926807ded89528ec20812002b0c49869
niroshan123 <niroshanratnayake07@gmail.com>
2020-04-29 23:55:25 +0530

    Basic Authentication setting up

--- Spring-Security-JWT/src/main/java/uom/niroshan/jwt/HelloWorldController.java
+++ Spring-Security-JWT/src/main/java/uom/niroshan/jwt/HelloWorldController.java
@@ -1,4 +1,14 @@
 package uom.niroshan.jwt;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/")
+    public String home(){
+        return("Hello User Welcom To Jungle");
+    }
+
 }
